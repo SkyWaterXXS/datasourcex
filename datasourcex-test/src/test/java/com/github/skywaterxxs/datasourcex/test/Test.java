4136dc103b3d122b6abf64c6c7a7696b451e5f89
xiaoshuo.xu <xiaoshuo.xxs@alibaba-inc.com>
2018-10-16 19:35:48 +0800

    first commit

---
+++ datasourcex-test/src/test/java/com/github/skywaterxxs/datasourcex/test/Test.java
@@ -0,0 +1,13 @@
+package com.github.skywaterxxs.datasourcex.test;
+
+/**
+ * <p>ClassName:com.github.skywaterxxs.datasourcex.test.Test</p>
+ * <p>描述:  </p>
+ * <p>日期: 2018/10/16 </p>
+ *
+ * @author xiaoshuo.xxs
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class Test {
+}
